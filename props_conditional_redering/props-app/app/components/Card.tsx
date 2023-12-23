f7d571852edf8b7f46d973dafae5de344ade7339
contactrehan <contactrehan2209@gmail.com>
2023-12-23 23:11:30 +0500

    changed filename

--- props_conditional_redering/props-app/app/components/Card.tsx
+++ props_conditional_redering/props-app/app/components/Card.tsx
@@ -1,17 +1,16 @@
 // components/Card.js
 "use client"
 import React from 'react';
-import {Card} from "@/types/componentTypes"
-const CardComponent = (prop:Card) => {
+import {CardType} from "@/types/componentTypes"
+import Tag from './tag';
+const CardComponent = (prop:CardType) => {
   return (
     <div className="bg-white shadow-md rounded-md p-6 mb-4">
       <h2 className="text-xl font-semibold mb-4">{prop.title}</h2>
       <p className="text-gray-600 mb-4">{prop.description}</p>
       <div className="flex flex-wrap">
         
-          <span  className="bg-gray-200 text-gray-700 rounded-full px-3 py-1 text-sm mr-2 mb-2">
-            {prop.tag}
-          </span>
+         <Tag tagtext={prop.tag}/>
         
       </div>
     </div>
---
+++ props_conditional_redering/props-app/types/componentTypes.ts
@@ -0,0 +1,17 @@
+type CardType =
+{
+    title:string,
+    description:string,
+    tag:string
+}
+export type {CardType}
+
+type ButtonType = {
+    label: string
+}
+export type {ButtonType}
+
+type TagType = {
+    tagtext: string
+}
+export type {TagType}
